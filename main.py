da322218817a65cadfaea0e7c969797a25966209
Radu Predescu <radu637@gmail.com>
2024-12-10 22:09:30 +0200

    added noise and filtering

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-from utils import loader,size,histogram
-import matplotlib.pyplot as plt
+from utils import (
+    loader,
+    size,
+    transform_to_gray,
+    histogram,
+    add_gaussian_noise,
+    apply_filter,
+    mse,
+)
+from configuration import Config, read_params
 import cv2
-import numpy as np
 
-if __name__ == '__main__':
-    path = str(input('Enter path: '))
-    image = loader(path)
-    image_shape = size(image)
-    
-    #firstly, we want to save our image, to see if it is loaded properly    
-    cv2.imwrite('saved_image.png',image)
 
-    histogram(image, 'color')
-    
+if __name__ == "__main__":
+    """
+    Imaginea incarcata trebuie sa fie neaparat RGB, altfel programul nu va merge :)
+    """
+    params = read_params("config.yaml")
+    config = Config(params=params)
+
+    path = str(input("Enter path: "))
+    raw_image = loader(path)
+    raw_image_shape = size(raw_image)
+
+    # verificam daca am incarcat o imagine rgb si afisam dimensiunea
+    assert len(raw_image_shape) == 3 and raw_image_shape[2] == 3, "Nu este rgb"
+    print("Dimensiunea imaginii initiale este: {}".format(raw_image_shape))
+
+    # salvam imaginea ca sa fim siguri ca am incarcat-o corect
+    cv2.imwrite("loaded_image.png", raw_image)
+
+    # afisam histograma pentru imaginea initiala
+    histogram(raw_image, 'color')
+
+    # transformam imaginea in gri
+    gray_image = transform_to_gray(raw_image)
+
+    # afisam dimensiunea imaginii gri
+    # daca am procedat corect, ar trebui sa vedem lipsa unei dimensiuni sau valoarea 1 in loc de 3
+    gray_image_shape = size(gray_image)
+    print("Dimensiunea imaginii gri este: {}".format(gray_image_shape))
+    cv2.imwrite("gray_image.png", gray_image)
+
+    # afisam histograma pentru imaginea gri
+    histogram(gray_image, 'gri')
+
+    img_with_noise = add_gaussian_noise(gray_image, gray_image_shape, config)
+    histogram(img_with_noise, 'gri cu zgomot')
+
+    filtered_image, _ = apply_filter(img_with_noise, 20)
+
+    cv2.imwrite("rectified.png", filtered_image)
+    histogram(filtered_image,'gri cu zgomot, filtrata')
+
+    print("MSE pentru imaginea gri si imaginea cu zgomot: {}".format(mse(gray_image, img_with_noise, 3)))
+    print("MSE pentru imaginea gri si imaginea filtrata: {}".format(mse(gray_image, filtered_image, 3)))
--- utils.py
+++ utils.py
@@ -2,20 +2,112 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def loader(path: str):
     """
     cv2 method to load our image
     """
-    return cv2.imread(path,cv2.IMREAD_ANYCOLOR)
+    return cv2.imread(path, cv2.IMREAD_ANYCOLOR)
 
-def size(image: cv2):
+
+def size(image: np.ndarray):
     """
     Returns the shape attribute of our cv2 image
     """
     return image.shape
 
-def histogram(image: cv2, purpose: str):
-    
-    plt.hist(image, bins='auto')
-    plt.title('Histogram for {}'.format(purpose))
-    plt.show()
+
+def transform_to_gray(image: np.ndarray):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def add_gaussian_noise(gray_image, gray_image_shape, config):
+    sigma = config.zgomot.dispersie
+    medie = config.zgomot.medie
+    n = np.random.normal(medie, sigma, gray_image_shape)
+    img_with_noise = gray_image + n
+    cv2.imwrite("image_with_noise.png", img_with_noise)
+    img_with_noise = np.clip(img_with_noise, 0, 255).astype(np.uint8)
+    return img_with_noise
+
+
+def histogram(image: np.ndarray, purpose: str):
+    if purpose == "color":
+        colors = ("b", "g", "r")
+        for i, color in enumerate(colors):
+            hist = cv2.calcHist([image], [i], None, [256], [0, 256])
+            plt.plot(hist, color=color)
+            plt.xlim([0, 256])
+
+        plt.title("Histograma pentru imaginea {}".format(purpose))
+        plt.xlabel("Valoare pixel")
+        plt.ylabel("Frecventa")
+        plt.show()
+
+    elif (
+        purpose == "gri cu zgomot"
+        or purpose == "gri"
+        or purpose == "gri cu zgomot, filtrata"
+    ):
+        hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+        plt.figure()
+        plt.title("Histograma pentru imaginea {}".format(purpose))
+        plt.xlabel("Valoare pixel")
+        plt.ylabel("Frecventa")
+        plt.plot(hist, color="black")  # Plot in black for grayscale
+        plt.xlim([0, 256])  # Pixel intensity range
+        plt.grid()
+        plt.show()
+
+
+def apply_filter(img, size):
+    h, w = img.shape
+    capat = size // 2
+    # pregatire imagine noua cu zero
+    new_img = np.zeros([h, w])
+    POS = np.zeros([h, w])
+    # parcurgerea imaginii ( atentie la capete )
+    for i in range(capat, h - capat):
+        for j in range(capat, w - capat):
+            # extrage vecinatate
+            vec = img[i - capat : i + capat + 1, j - capat : j + capat + 1]
+            # populeaza imaginea noua cu rezultatul filtrarii
+            new_img[i, j], POS[i, j] = adaptiv_orientat(vec, size)
+    new_img = np.uint8(new_img)
+    # intoarce noul rezultat
+    return new_img, POS
+
+
+def adaptiv_orientat(vec, size):
+    center = size // 2
+    # medie orizontala ( 0 grade )
+    med0 = np.sum(vec[center, :]) / size
+    # medie verticala ( 90 grade )
+    med90 = np.sum(vec[:, center]) / size
+    # medie diagonala 45 grade
+    med45 = 0.0
+    for i in range(0, size):
+        med45 = med45 + vec[i, size - 1 - i]
+    med45 = med45 / size
+    # medie diagonala 135 grade
+    med135 = 0.0
+    for i in range(0, size):
+        med135 = med135 + vec[i, i]
+    med135 = med135 / size
+    # calculam diferentele
+    # functia pentru modul ( valoare absoluta) este abs()
+    temp = np.array([med0, med90, med45, med135])
+    diff = np.abs(temp - vec[center, center])
+    pos = np.argmin(diff)
+    return temp[pos], pos
+
+
+def mse(img, img_modif, capat):
+    h, w = img.shape
+    delta = (
+        img[capat : h - capat, capat : w - capat]
+        - img_modif[capat : h - capat, capat : w - capat]
+    )
+    delta = delta**2
+    return np.sum(delta) / ((h - capat * 2) * (w - capat * 2))
